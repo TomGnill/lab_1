8b4fd88964646a48043b89b4fe15cfb2aead77f2
TomGnill <s.drun@yandex.ru>
2020-09-23 15:28:28 +0300

    1.1

---
+++ calc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+calc() {
+  sum() {
+    echo $(($2 + $3))
+  }
+  sub() {
+    echo $(($2 - $3))
+  }
+  mul() {
+    echo $(($2 - $3))
+  }
+  div() {
+    echo "$2/$3" |bc -l
+  }
+}
