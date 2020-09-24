31eb4bb0c50699eb6635c016647964f31a3857c0
TomGnill <s.drun@yandex.ru>
2020-09-24 19:25:58 +0300

    1.4

--- strlen.sh
+++ strlen.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 strlen() {
+if ! [[ $# -eq 2 ]]; then
+echo"Error: для правильной работы укажите строку, если строка содержит пробел попробуйте добавить ковычки">&2; exit -3
+fi
    somevar="$1"
    size=${#somevar}
    echo "$size"
