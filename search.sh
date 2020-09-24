f2870c388d1da46b05c711c8a5fb9c3549108d04
TomGnill <s.drun@yandex.ru>
2020-09-24 19:25:46 +0300

    1.2

--- search.sh
+++ search.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-search() {
-      
+search(){
+if ![[ $# -eq 3]]; then
+echo"Error: для корректной работы search необходимо ввести слово и название директории">&2; exit -1
+fi
+if ![ -d "$3" ]; then 
+echo"Error: такой директории не существует."
+fi
   grep -r "$1" "$2"
 
 }
