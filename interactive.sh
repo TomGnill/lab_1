cfd73e2d6ba02bd4fd4dab7babb83ad545ff3cd0
TomGnill <s.drun@yandex.ru>
2020-09-24 23:35:19 +0300

    1.8

--- interactive.sh
+++ interactive.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 interactive() {
-echo" Вас приветствует интерактивный режим labOS, не пытайтесь меня сломать, ибо всё что я умею
-в этой жизни это дышать(
-Введите букву для выбора действия, или его название:
+echo "Вас приветствует интерактивный режим labOS, не пытайтесь меня сломать, ибо всё что я умею
+ в этой жизни это дышать
 
-c\ -"calc"
-s\ -"search"
-r\ -"reverse"
-n\ -"strlen"
-l\ -"log"
-для подробного описания введите \h -"help"
-Для выхода из программы e\ -"exit"
-"
+Введите букву для выбора действия, или его название:
+ c\ - "calc"
+ s\ - "search"
+ r\ - "reverse"
+ n\ - "strlen"
+ l\ - "log"
+ для подробного описания введите h -help
+ Для выхода из программы e - exit
+ "
 read command
 case $command in
  c|calc)
@@ -22,10 +22,9 @@ case $command in
  echo "выберите действие:sum(+)/sub(-)/mul(*)/div(/):"; read calc_command
  echo "первое число:"; read first
  echo "второе число:"; read second
- calc "$calc_command" "$first" "$second"
- fi
-echo " "
-interactive
+ echo "Ответ:"; calc "$calc_command" "$first" "$second"
+ echo " "
+ interactive
 ;;
  s|search)
   if [ "$sea_er" = "1" ]; then
@@ -52,6 +51,44 @@ fi
   reverse "$rev_dir1" "$rev_dir2"
   echo" "
   interactive
+;;
+ n|strlen)
+  if [ "$str_er" = "1" ]; then
+   echo "Error: действие недоступно" >&2;
+   interactive
+  fi
+   echo"Введите строку:"; read string
+   strlen  "$string"
+   echo " "
+   interactive
+;;
+ l|log)
+  if [ "$log_er" = "1" ]; then
+   echo"Error: действие недоступно" >&2;
+   interactive
+  fi
+    log "var/log/anaconda/X.log"
+    echo " "
+    interactive
+;;
+ e|exit)
+  if [ "$exi_er" = "1" ]; then
+  echo "Error: действие недоступно" >&2;
+  interactive
+  fi
+   command exit 0
+;;
+ h|help)
+  if [ "$hel_er" = "1"]; then
+   echo "Error: действие недоступно" >&2;
+   interactive
+  fi
+   help
+   interactive
+;;
+ *)
+   echo "Вы выбрали несуществующее действие" >&2;
+   interactive
 ;;
 esac
 }
