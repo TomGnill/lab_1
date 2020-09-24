a935b41b438abe1c5f4b9096fd1e2fc86412d58a
TomGnill <s.drun@yandex.ru>
2020-09-24 19:25:32 +0300

    1.0

--- main.sh
+++ main.sh
@@ -1,100 +1,100 @@
 #!/bin/bash
-if ! [ -f "calc.sh"]; then
+if ! [ -f "calc.sh" ]; then
    calc_er=1
-else
+ else
 . ./calc.sh
 fi
-if ! [-f"search.sh"]; then
+if ! [ -f "search.sh" ]; then
     sea_er=1
-else
+ else
 . ./search.sh
 fi
-if ![ -f "reverse.sh"]; then
+if ! [ -f "reverse.sh" ]; then
     rev_er=1
-else
+ else
 . ./reverse.sh
 fi
-if ! [ -f "strlen.sh"]; then
+if ! [ -f "strlen.sh" ]; then
     str_er=1
-else
+ else
 . ./strlen.sh
 fi
-if ! [ -f "log.sh"]; then
+if ! [ -f "log.sh" ]; then
     log_er=1
-else
+ else
 . ./log.sh
 fi
-if ! [ -f "exit.sh"]; then
+if ! [ -f "exit.sh" ]; then
     exi_er=1
-else
+ else
 . ./exit.sh
 fi
-if ! [ -f "interactive.sh"]; then
+if ! [ -f "interactive.sh" ]; then
    int_er=1
-else
+ else
 . ./interactive.sh
 fi
-if ! [ -f "help.sh"]; then
+if ! [ -f "help.sh" ]; then
    hel_er=1
-else
+ else
 . ./help.sh
 fi
 case $1 in
 calc)
-if ["$calc_er" = "1"]; then
+if [ "$calc_er" = "1" ]; then
  echo" Error:такого действия нет!">&2; exit -5
 fi
     calc "$2" "$3" "$4"
     exit 0
 ;;
 search)
-if ["$sea_er" = "1"]; then
+if [ "$sea_er" = "1" ]; then
  echo" Error:такого действия нет!">&2; exit -5
 fi
     search "$2" "$3"
     exit 0
 ;;
 reverse)
-if ["$rev_er" = "1"] then
+if [ "$rev_er" = "1" ]; then
  echo"Error: такого действия нет!">&2; exit -5
 fi
     reverse" $2" "$3"
     exit 0
 ;;
 strlen)
-if ["str_er" ="1"] then
- echo"Error: такого действия нет!">&2 exit-5
+if [ "str_er" ="1" ]; then
+ echo"Error: такого действия нет!">&2; exit -5
 fi
     strlen "$2"
     exit 0
 ;;
 log)
-if ["log_er" ="1"] then
- echo"Error: такого действия нет!">&2 exit -5
+if [ "log_er" ="1" ]; then
+ echo"Error: такого действия нет!">&2; exit -5
 fi
     log "var/log/anaconda/X.log"
     exit 0
 ;;
 exit)
- if ["$er_ex" = "1"]; then
+if [ "$er_ex" = "1" ]; then
    echo"Error: данное действие недоступно.">&2 ;exit -5
 fi
- if! [[ $# -eq 1]]; then
+ if ! [[ $# -eq 1 ]]; then
    exit "$2"
 else
    exit "0"
 fi
 ;;
 interactive)
- if["$int_er" = "1"]; then
+ if [ "$int_er" = "1" ]; then
   echo"Error: данное действие недоступно.">&2; exit -5
 fi
   interactive
 ;;
 help)
- if[ "$hel_er" = "1" ]; then
-echo "Error: Действие недоступно">&2;exit -5
-fi 
+ if [ "$hel_er" = "1" ]; then
+echo "Error: Действие недоступно">&2; exit -5
+fi
  help
  exit 0
 ;;
