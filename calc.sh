cb232410e5bd1e81ab0363954e9941a01d6484ab
TomGnill <s.drun@yandex.ru>
2020-09-24 23:34:27 +0300

    1.1

--- calc.sh
+++ calc.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
  calc() {
-function sum(){
-echo $(($3 + $4))
-}
-function sub(){
-echo $(($3 - $4))
-}
-function mul(){
-echo $(($3 * $4))
-}
+
  re='^-?[0-9]+$'
- if ! [[ $# -eq 4 ]];then
+ if ! [[ $# -eq 3 ]];then
    echo "Error: количество аргументов слишком большое/мало">&2; exit -4
  fi
- if [[ "$2" != "sum" && "$2" != "mul" && "$2" != "sub" && "$2" != "div" ]]; then
+ if [[ "$1" != "sum" && "$1" != "mul" && "$1" != "sub" && "$1" != "div" ]]; then
    echo "Error: такого действия нет" >&2; exit  -5
  fi
- if ! [[ "$3" =~ $re && "$4" =~ $re ]]; then
-   echo"Error: для корректной работы calc необходимо ввести 2 целых числа." >&2; exit -4
+ if ! [[ "$2" =~ $re && "$3" =~ $re ]]; then
+   echo "Error: для корректной работы calc необходимо ввести 2 целых числа." >&2; exit -1
  fi
- if [[ "$2" == "div" && "$4" == "0" || "-0" ]]; then
-   echo"Error: На ноль делить нельзя!" >&2; exit -4
- fi
-case $2 in
+ if [[ "$1" == "div" && "$2" == "0" ]]; then
+   echo "Error: На ноль делить нельзя!" >&2; exit -1
+fi
+case $1 in
  sum)
-  sum
+  echo $(($2 + $3))
  ;;
  sub)
-  sub
+  echo $(($2 - $3))
  ;;
  mul)
-  mul
+  echo $(($2 * $3))
  ;;
  div)
-   div"$3/$4" | bc -l
+  echo $(($2 / $3)) | bc -l
  ;;
   esac
 }
