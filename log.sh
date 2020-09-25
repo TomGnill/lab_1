260c7bbd50989f01f32eb3aafc11e3f780b921ae
TomGnill <s.drun@yandex.ru>
2020-09-25 13:20:28 +0300

    1.8

--- log.sh
+++ log.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 log() {
-if ! [ -f "var/log/anaconda/X.log" ]; then
+if ! [ -f "/var/log/anaconda/X.log" ]; then
   echo "Error: log файла не существует" >&2; exit -3
+else
+cat /var/log/anaconda/X.log |grep "(WW)"|grep -v "warning"|sed "s/(WW)/\x1b[93mWarning:\x1b[0m/g"
+cat /var/log/anaconda/X.log |grep "(II)"|grep -v "informational"|sed "s/(II)/\x1b[36mInformation:\x1b[0m/g"
 fi
-while IFS = read -r line
-do
- if [[ (("$line" == *"(WW)"*)) && ! (("$line" == *"(WW) warning, (EE) error, (NI) not implemented,
-(??) unknown"*)) ]]; then
- echo -e "${line//"(WW)"/"\e[33mWarning:\e[0m"}"
-fi
-done <"$1"
-while IFS = read -r line
-do if [[ (("$line" == *"(II)")) && ! (("$line" == *"(++) from command line, (!!) notice, (II)
-informatioanl,"*)) ]]; then
- echo -e "${line//"(II)"/"\e[94mInformation:\e[0m"}"
-fi
- done < "$1"
 }
