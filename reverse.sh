225be23fd0462e6f28c9dc0302b0d7f96e9e324e
TomGnill <s.drun@yandex.ru>
2020-09-25 18:07:59 +0300

    1.3

--- reverse.sh
+++ reverse.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 reverse() {
 if ! [[ -f "$1" ]]; then
-echo "Error: необходимо указать input file">&2; exit -1
+echo "Error: файл не существует">&2; exit -1
 fi
 if ! [[ -f "$2" ]] ; then
-echo "Error: фалйл не существует">&2; exit -1
+echo "Error: файл не существует">&2; exit -1
 fi
  touch $2 &> /dev/null
  tac $1 > $2
