2250897510564ce934ea64c59b323b5a34e4dc6a
TomGnill <s.drun@yandex.ru>
2020-09-25 14:53:25 +0300

    1.3

--- reverse.sh
+++ reverse.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 reverse() {
-if ! [[ $3 -eq 3 ]]; then
-echo"Error: необходимо указать input iutpu директорию">&2; exit -1
+if ! [[ $# -eq $3 ]]; then
+echo "Error: необходимо указать input директорию">&2; exit -1
 fi
 if ! [[ -f "$2" ]] ; then
-echo"Error: фалйл не существует">&2; exit -1
+echo "Error: фалйл не существует">&2; exit -1
 fi
-
  IFC=$'\n'
 for entry in $(tac "$1")
 do
 for value in $entry
 do
-echo " $value"
- done
+echo " $value" | awk' {for (i=NF; i>1: i--) printf("%s ".$i);
+   printf $1; }' >> "$2"
+done
 done
 }
